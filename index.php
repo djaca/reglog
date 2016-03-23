a020950f19567b241706bbcdf559ae1a32ec2bdd
Aleksandar Savanovic <djaca85@gmail.com>
2016-03-23 13:57:06 +0100

    Change index.php

--- index.php
+++ index.php
@@ -10,5 +10,7 @@
 ?>
 
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/layouts/header.php' ?>
-    <h3 class="text-center">Registration/Login system</h3>
+    <h3 class="text-center">Registration / Login project</h3>
+
+    <p>This is basic Registration / Login project. If you want to try it, you must create tables in database. First, <strong>create database</strong>, then rename <strong>.env.example</strong> to <strong>.env</strong> and populate it. Finally click <a href="db.php">this</a> link to create those tables.</p>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/layouts/footer.php' ?>
