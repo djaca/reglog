8eeb75d812ff7479c5b62bc4738a22e9d68f4dbc
Aleksandar Savanovic <djaca85@gmail.com>
2016-03-23 16:03:55 +0100

    Add instruction for medoo

--- index.php
+++ index.php
@@ -13,4 +13,5 @@
     <h3 class="text-center">Registration / Login project</h3>
 
     <p>This is basic Registration / Login project. If you want to try it, you must create tables in database. First, <strong>create database</strong>, then rename <strong>.env.example</strong> to <strong>.env</strong> and populate it. Finally click <a href="db.php">this</a> link to create those tables.</p>
+    <p>Change medoo.php, line 631, from PDO::FETCH_ASSOC to PDO::FETCH_OBJ!! </p>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/layouts/footer.php' ?>
