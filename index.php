f502aa1d592900297a48c4a210a5c4c9003d68b3
Aleksandar Savanovic <djaca85@gmail.com>
2016-03-23 13:26:17 +0100

    Change index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-    
+    use Acme\User;
+
+    require $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
+
+    if(isLoggedIn()) {
+        $user = new User();
+    }
+
+?>
+
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/layouts/header.php' ?>
+    <h3 class="text-center">Registration/Login system</h3>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/layouts/footer.php' ?>
